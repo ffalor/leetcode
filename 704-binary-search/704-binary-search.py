f59d239894913c545be6a31ed20f82625f99aba9
ffalor <35144141+ffalor@users.noreply.github.com>
2022-01-05 23:45:13 -0600

    Time: 283 ms (16.52%), Space: 14.7 MB (57.14%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,25 +1,12 @@
-class Solution(object):
+class Solution:
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        
         left, right = 0, len(nums) - 1
-
         while left <= right:
-            
-            pivot = left + (right-left)//2
-                
+            pivot = left + (right - left) // 2
             if nums[pivot] == target:
                 return pivot
-            
-            if nums[pivot] < target:
-                left = pivot + 1
-            else:
+            if target < nums[pivot]:
                 right = pivot - 1
-                
-                
-            
+            else:
+                left = pivot + 1
         return -1
