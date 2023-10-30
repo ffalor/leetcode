26464edda9885b0118cac65e5f493c2f2d401f3f
ffalor <35144141+ffalor@users.noreply.github.com>
2023-10-29 20:04:58 -0500

    Time: 1 ms (82.69%), Space: 1.9 MB (93.85%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.go
@@ -0,0 +1,23 @@
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	left, right := 0, n
+
+	for left < right {
+		pivot := (left + right) / 2
+
+		if isBadVersion(pivot) {
+			right = pivot
+		} else {
+			left = pivot + 1
+		}
+	}
+
+	return left
+}
